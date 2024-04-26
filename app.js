e67f5289dff9a5ab98e1f5a9a244397136ca4ac3
rajeevprajapatt <rajeevprajapat06@gmail.com>
2024-04-26 22:13:36 +0530

    style.css file added

---
+++ app.js
@@ -0,0 +1,58 @@
+// targeting all the number buttons
+let numbers = document.querySelectorAll(".items");
+
+// targeting the display where output will shown 
+let display = document.querySelector("#text");
+
+// targeting = button to evaluate expression
+let exx = document.querySelector("#exx");
+
+// targeting the clear button to clear all the display values
+let AC_btn = document.querySelector(".AC_btn");
+
+// targeting the delete button to delete the input numbers 
+let delButton = document.querySelector("#delete_button");
+
+// creating an array where input values are stored
+let arr = [];
+
+const calculate = (expp) => {
+    let ans = eval(expp);
+    return ans;
+}
+
+// creating a function for delete button to delete input numbers
+delButton.addEventListener('click', () => {
+    arr.pop();
+    display.innerText = arr.join("");
+})
+
+numbers.forEach((number) => {
+    number.addEventListener("click", () => {
+
+        let x = number.innerText;
+        arr.push(x);
+
+        let expp = arr.join("");
+        display.innerText = arr.join("");
+
+        let result = calculate(expp);
+
+        exx.addEventListener("click", () => {
+            display.innerText = `${result}`;
+            arr.length = 0;
+        })
+        if (arr.length > 13) {
+            display.innerText = "Out of range";
+            arr.length = 0;
+        }
+    })
+})
+
+AC_btn.addEventListener("click", () => {
+    display.innerText = "";
+    arr.length = 0;
+})
+
+
+67
